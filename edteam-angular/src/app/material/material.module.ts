5356b572d894661355c0b3406a21ee202488c711
Gerardo Alderete <gerardoalderete92@gmail.com>
2018-12-05 15:18:18 -0600

    PRIMER COMPONENTE TOOLBAR

--- edteam-angular/src/app/material/material.module.ts
+++ edteam-angular/src/app/material/material.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 import { MaterialDemoComponent } from './material-demo/material-demo.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule
+  ],
+  exports: [
+    MatToolbarModule
   ],
   declarations: [MaterialDemoComponent]
 })
