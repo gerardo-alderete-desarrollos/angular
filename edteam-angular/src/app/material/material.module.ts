baafecb0581296b55a51c25bb20b2f4a20860704
Gerardo Alderete <gerardoalderete92@gmail.com>
2018-12-09 11:42:53 -0600

    GENERAR COMPONENTES INPUT-FIELDS

--- edteam-angular/src/app/material/material.module.ts
+++ edteam-angular/src/app/material/material.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import {MatToolbarModule,
         MatButtonModule,
         MatIconModule,
-        MatExpansionModule
+        MatExpansionModule,
+        MatInputModule
 } from '@angular/material';
 
 import { MaterialDemoComponent } from './material-demo/material-demo.component';
 import { MaterialButtonComponent } from './material-button/material-button.component';
+import { MaterialInputComponent } from './material-input/material-input.component';
 
 @NgModule({
   imports: [
@@ -15,14 +17,18 @@ import { MaterialButtonComponent } from './material-button/material-button.compo
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatInputModule
+
   ],
   exports: [
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatInputModule
+
   ],
-  declarations: [MaterialDemoComponent, MaterialButtonComponent]
+  declarations: [MaterialDemoComponent, MaterialButtonComponent, MaterialInputComponent]
 })
 export class MaterialModule { }
