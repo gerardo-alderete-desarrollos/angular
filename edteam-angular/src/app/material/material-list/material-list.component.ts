ba235264d19c699359d19225b6758b981ac2c08d
Gerardo Alderete <gerardoalderete92@gmail.com>
2018-12-09 12:16:52 -0600

    DEFINIENDO UN MODELO DE DATOS

--- edteam-angular/src/app/material/material-list/material-list.component.ts
+++ edteam-angular/src/app/material/material-list/material-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Estudiante } from '../../shared/model/estudiante';
 
 @Component({
   selector: 'ed-material-list',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./material-list.component.scss']
 })
 export class MaterialListComponent implements OnInit {
+  estudiante: Estudiante;
+  estudiante2: Estudiante;
+  estudiante3: Estudiante;
 
   constructor() { }
 
   ngOnInit() {
+    console.log('Inicializando el componente MaterialList');
+    this.estudiante = {
+      id: 1,
+      nombre: 'Luis Aviles',
+      ciudad: 'Cochabamba',
+      fotoURL: 'http://mipagina.com/foto.jpg'
+    }
+
+    this.estudiante2 = {
+      id: 2,
+      nombre: 'Jorge Perez',
+      ciudad: 'Lima'
+    }
+
+    this.estudiante3 = {
+      id: 3,
+      nombre: 'Maria Rodriguez',
+      ciudad: 'Bogota'
+    }
   }
 
 }
